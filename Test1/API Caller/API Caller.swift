1588413755a5b2dc74f5f68c2920fc197efd380c
DBerdnikovO <95565575+DBerdnikovO@users.noreply.github.com>
2022-08-19 14:29:37 +0400

    l

--- Test1/API Caller/API Caller.swift
+++ Test1/API Caller/API Caller.swift
@@ -18,28 +18,28 @@ enum APIError: Error {
     case failedTogetData
 }
 
-//class APICaller {
-//    
-//    static let shared = APICaller()
-//    
-//    func getTrendingMovies(completion: @escaping (Result<[Title], Error>) -> Void) {
-//        print("ITS TRANDING MOVIE")
-//        guard let url = URL(string: "\(Constants.baseURL)/3/trending/movie/day?api_key=\(Constants.API_KEY)") else {return}
-//        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
-//            guard let data = data, error == nil else {
-//                return
-//            }
-//
-//            do {
-//                let results = try JSONDecoder().decode(Video.self, from: data)
-//                completion(.success(results.results))
-//                
-//            } catch {
-//                completion(.failure(APIError.failedTogetData))
-//            }
-//        }
-//        
-//        task.resume()
-//    }
-//    
-//}
+class APICaller {
+    
+    static let shared = APICaller()
+    
+    func getTrendingMovies(completion: @escaping (Result<[Title], Error>) -> Void) {
+        print("ITS TRANDING MOVIE")
+        guard let url = URL(string: "\(Constants.baseURL)/3/trending/movie/day?api_key=\(Constants.API_KEY)") else {return}
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+
+            do {
+                let results = try JSONDecoder().decode(Video.self, from: data)
+                completion(.success(results.results))
+                
+            } catch {
+                completion(.failure(APIError.failedTogetData))
+            }
+        }
+        
+        task.resume()
+    }
+    
+}
--- Test1/Extensions/Bundle.swift
+++ Test1/Extensions/Bundle.swift
@@ -8,23 +8,6 @@
 import Foundation
 import UIKit
 
-//extension Bundle {
-//    func decode<x: Decodable>(_ type: x.Type, from filename: String) -> x? {
-//            guard let json = url(forResource: filename, withExtension: nil) else {
-//                print("Failed to locate \(filename) in app bundle.")
-//                return nil
-//            }
-//            do {
-//                let jsonData = try Data(contentsOf: json)
-//                let decoder = JSONDecoder()
-//                let result = try decoder.decode(x.self, from: jsonData)
-//                return result
-//            } catch {
-//                print("Failed to load and decode JSON \(error)")
-//                return nil
-//            }
-//        }
-//}
 
 extension Bundle {
     func decode<T: Decodable>(_ type: T.Type, from file: String) -> T {
